d38adc5ca963441b6ce77c016f9725ca536390fa
ba hanlon <herbivore137@gmail.com>
2019-04-19 08:24:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,6 +25,7 @@ def swap_elements(arr)
   arr
 end
 
+<<<<<<< HEAD
 # Question 4 Advanced
 def swap_elements_from_to(array, index, destination_index)
   temp = array[index]
@@ -33,6 +34,8 @@ def swap_elements_from_to(array, index, destination_index)
   array
 end
 
+=======
+>>>>>>> 3a3fd5ce94628d04fdfb616de55d725be1b2ac25
 # Question 5
 def reverse_array(int_arr)
   int_arr.reverse
@@ -54,6 +57,7 @@ def find_a(arr)
   end
 end
 
+<<<<<<< HEAD
 # Question 8 (Advanced) 
 def sum_array(int_arr)
  int_arr.inject do |sum, n|
@@ -66,4 +70,33 @@ def add_s(arr)
   arr.collect!.each_with_index do |element, index|
     index != 1 ? element + "s" : element
   end
+=======
+# Question 8
+def sum_array(int_arr)
+  sum = 0
+  int_arr.each do |int|
+    sum += int
+  end
+  sum
+end
+
+# Question 9
+#def add_s(arr)
+# arr.each do |word|
+#    if word != arr[1]
+#      new_arr << word + "s"
+#    else
+#      new_arr << word
+#    end
+#  end
+#  new_arr
+#end
+
+def add_s(arr)
+  arr.each_with_index.collect do |element, index|
+    if index != 1
+      element = element + "s"
+  end
+  arr
+>>>>>>> 3a3fd5ce94628d04fdfb616de55d725be1b2ac25
 end
